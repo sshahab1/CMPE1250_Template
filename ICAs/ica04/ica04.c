0e84e0ca6eb7cf2fb6f432afb3c308176757d7ba
Saamia Shahab <sshahab1@nait.ca>
2024-01-29 18:21:56 -0700

    worked on part 2

--- ICAs/ica04/ica04.c
+++ ICAs/ica04/ica04.c
@@ -1,12 +1,13 @@
 /********************************************************************/
-// HC12 Program:  YourProg - MiniExplanation
+// HC12 Program:  ICA 04 using ossciliscope and waveform to measure 
 // Processor:     MC9S12XDP512
 // Bus Speed:     40 MHz
 // Author:        Saamia
 // Details:       A more detailed explanation of the program is entered here               
-// Date:          Date Created
+// Date:          Jan 29
 // Revision History :
-//  each revision will have a date + desc. of changes
+//  created ica04 
+//worked on part 1 and 2 
 
 
 /********************************************************************/
@@ -33,6 +34,8 @@ void RED (int b0n);
 // Global Variables
 /********************************************************************/
 uint16_t uiMainLoopCount = 0;
+unsigned int i = 0;
+unsigned long j = 0;
 /********************************************************************/
 // Constants
 /********************************************************************/
@@ -65,6 +68,10 @@ void main(void)
   for (;;)
 {
     PT1AD1 ^= RED_LED
+    for(i=0;i<500000;i++)
+    {
+        PT1AD1 &= RED_LED
+    }
   //++uiMainLoopCount;
   //RED(uiMainLoopCount<0x1000);
 }            
