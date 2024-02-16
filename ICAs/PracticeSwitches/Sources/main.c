07cdf35c0524f177c929f32a0a4829f6de46a876
Saamia Shahab <sshahab1@nait.ca>
2024-02-16 11:42:28 -0700

    commit practice

--- ICAs/PracticeSwitches/Sources/main.c
+++ ICAs/PracticeSwitches/Sources/main.c
@@ -2,9 +2,9 @@
 // HC12 Program:  YourProg - MiniExplanation
 // Processor:     MC9S12XDP512
 // Bus Speed:     40 MHz
-// Author:        Saamia
+// Author:        Saamia Shahab
 // Details:       A more detailed explanation of the program is entered here
-// Date:          Date Created
+// Date:          Feb 16 2024
 // Revision History :
 //  each revision will have a date + desc. of changes
 
