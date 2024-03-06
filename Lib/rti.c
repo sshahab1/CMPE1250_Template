1cd142d24b364392621a376cd5ad7f254ef17d7d
Saamia Shahab <144168528+sshahab1@users.noreply.github.com>
2024-03-06 09:54:13 -0700

    commit rti

--- Lib/rti.c
+++ Lib/rti.c
@@ -14,20 +14,18 @@ void RTI_InitCallback(void(*function)(void))
 
 void RTI_Delay_ms(unsigned int ms)
 {
-    RTICTL=0; //stop the RTI, shouldn’t be running
-
-    if (CRGFLG_RTIF) // RTI period over?
+    if (CRGFLG_RTIF) // Stop the RTI 
     {
-        CRGFLG = CRGFLG_RTIF_MASK; //clear flag, VERY IMPORTANT 
-        
-    }
-    RTICTL = 0b10010111; //1ms 16*10^3 o 16MHz / 16000 = 1000Hz, or 1ms.
-    unsigned int delay = ms * RTICTL;
-    if (CRGFLG_RTIF) // RTI period over?
-    {
-        CRGFLG = CRGFLG_RTIF_MASK; //clear flag, VERY IMPORTANT 
+        CRGFLG ~= CRGFLG_RTIF_MASK; //Stop the RTI 
         
     }
     
-    RTICTL=0; 
+    while(i<ms)
+    {
+        if (CRGFLG_RTIF) // RTI period over?
+        {
+            CRGFLG = CRGFLG_RTIF_MASK; //clear flag, VERY IMPORTANT 
+            i++
+        }
+    }
 }
